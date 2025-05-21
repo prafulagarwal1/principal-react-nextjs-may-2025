b1caf2342c83fef40d10d1e81ce5b91bffb2268d
Prashanth Puranik <puranik@digdeeper.in>
2025-05-21 14:29:04 +0530

    local server

--- 03-typescript/src/15-generic-function.ts
+++ 03-typescript/src/15-generic-function.ts
@@ -7,6 +7,7 @@ const days = [
 ];
 
 // map()
+// [ 6, 7, 9, 8, 6 ]
 const mapResult = days.map(
     function (day) {
         return day.length;
@@ -15,3 +16,29 @@ const mapResult = days.map(
 console.log(mapResult);
 
 // implement our own map() function - generic one in TS
+type MapperFunction<T, U> = ( item : T ) => U;
+
+const map = <T, U>(arr : T[], mapper : MapperFunction<T, U>) => {
+    const result = [];
+
+    for( let i = 0; i < arr.length; ++i ) {
+        const mappedItem = mapper( arr[i] );
+        result.push( mappedItem );
+    }
+
+    return result;
+};
+
+// explicitly specify T and U bindings
+const numCharsInDays = map<string, number>(
+    days,
+    function (day) {
+        return day.length;
+    }
+);
+
+console.log( numCharsInDays );
+
+const primes = [ 2, 3, 5, 7, 11 ];
+
+console.log( map( primes, x => x * x ) );
