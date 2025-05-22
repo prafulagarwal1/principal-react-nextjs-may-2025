5145bcba6bb8526edf09989d3ac0aec02d051688
Prashanth Puranik <puranik@digdeeper.in>
2025-05-22 17:18:37 +0530

    STEP 17

--- 04-react/workshops-app/src/components/workshops/WorkshopsList/WorkshopsList.tsx
+++ 04-react/workshops-app/src/components/workshops/WorkshopsList/WorkshopsList.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router";
 
 import LoadingSpinner from "../../common/LoadingSpinner/LoadingSpinner";
 import ErrorAlert from "../../common/ErrorAlert/ErrorAlert";
+import Pagination from "../../common/Pagination/Pagination";
 
 import Item from "./Item/Item";
 
@@ -61,26 +62,13 @@ const WorkshopsList = () => {
             <hr />
 
             <div>
-                <Button
-                    variant="primary"
-                    size="sm"
-                    disabled={
-                        !(loading === false && error === null) || page === 1
-                    }
-                    onClick={(event) => previous(page - 1)}
-                    className="me-2"
-                >
-                    Previous
-                </Button>
-                <Button
-                    variant="primary"
-                    size="sm"
-                    disabled={!(loading === false && error === null)}
-                    onClick={() => next(page + 1)}
-                >
-                    Next
-                </Button>
-                <div>You are viewing page {page}</div>
+                <Pagination
+                    page={page}
+                    onPrevious={previous}
+                    onNext={next}
+                    disablePrevious={!(loading === false && error === null)}
+                    disableNext={!(loading === false && error === null)}
+                />
             </div>
 
             {
