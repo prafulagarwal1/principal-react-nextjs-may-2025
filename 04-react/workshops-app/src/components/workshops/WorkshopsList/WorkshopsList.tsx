58e5bea4fe88ef5f91278fb50cf552af3b4b7a08
Prashanth Puranik <puranik@digdeeper.in>
2025-05-22 15:25:39 +0530

    STEP 11

--- 04-react/workshops-app/src/components/workshops/WorkshopsList/WorkshopsList.tsx
+++ 04-react/workshops-app/src/components/workshops/WorkshopsList/WorkshopsList.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { Alert, Button, Col, Card, Row, Spinner } from "react-bootstrap";
+import { Link } from "react-router";
+
 import { getWorkshops } from "../../../services/workshops";
 import IWorkshop from "../../../models/IWorkshop";
 
@@ -40,13 +43,41 @@ const WorkshopsList = () => {
             <hr />
 
             {
-                workshops.length !== 0 ? (
-                    <div>{workshops.length}</div>
+                /* if..else like behavior using ? : */
+                loading ? (
+                    <div className="text-center">
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </div>
                 ) : (
-                    <div>Data not available</div>
+                    <div>Completed loading</div>
                 )
             }
 
+            {
+                /* if like behavior using && */
+                loading === false && error !== null && (
+                    <Alert variant="danger">{error.message}</Alert>
+                )
+            }
+
+            {
+                /*workshops.length !== 0 ? (
+                    <div>{workshops.length}</div>
+                ) : (
+                    <div>Data not available</div>
+                )*/
+            }
+            {
+                workshops.length !== 0 && (
+                    workshops.map(
+                        (workshop) => (
+                            <div key={workshop.id}>{workshop.name}</div>
+                        )
+                    )
+                )
+            }
         </div>
     );
 }
