79bc5a6f3590ea53fe5292eb313c68758b1862e1
Prashanth Puranik <puranik@digdeeper.in>
2025-05-23 17:29:57 +0530

    Step 34: Validation using react-hook-form

--- 04-react/workshops-app/src/components/workshops/WorkshopDetails/AddSession/AddSession.tsx
+++ 04-react/workshops-app/src/components/workshops/WorkshopDetails/AddSession/AddSession.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent } from 'react';
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 import { Level } from '../../../../models/ISession';
 
@@ -9,29 +10,45 @@ interface Props {
 }
 
 const AddSession = ( { id } : Props) => {
-    const [sequenceId, setSequenceId] = useState('1');
-    const [name, setName] = useState('');
-    const [speaker, setSpeaker] = useState('');
-    const [duration, setDuration] = useState('');
-    const [level, setLevel] = useState('Basic');
-    const [abstract, setAbstract] = useState('');
+    // register() -> { ref: useRef(), ... }
+
+    const { register, formState: { errors }, getValues } = useForm({
+        mode: 'all' // by default validations are triggered first time only after first submit. Now we are triggering on first blur, input
+    });
+
+    const validateDurationAndLevel = () => {
+        const duration = +getValues('duration');
+        const level = getValues('level');
+
+        if( level === 'Basic' && duration < 1 ) {
+            return 'Basic level shold have minimum 1 hour duration';
+        }
+
+        if( level === 'Intermediate' && duration < 2 ) {
+            return 'Intermediate level shold have minimum 2 hours duration';
+        }
+
+        if( level === 'Advanced' && duration < 3 ) {
+            return 'Advanced level shold have minimum 3 hours duration';
+        }
+    };
 
     const addSession = (event : FormEvent) => {
         event.preventDefault();
 
-        const session = {
-            workshopId: id,
-            upvoteCount: 0,
-            sequenceId: +sequenceId,
-            // name: name,
-            name,
-            speaker,
-            duration: +duration,
-            level: level as Level,
-            abstract,
-        };
+        // const session = {
+        //     workshopId: id,
+        //     upvoteCount: 0,
+        //     sequenceId: +sequenceId,
+        //     // name: name,
+        //     name,
+        //     speaker,
+        //     duration: +duration,
+        //     level: level as Level,
+        //     abstract,
+        // };
 
-        console.log( session );
+        // console.log( session );
 
         // You can do validation here, and submit the form if valid
         // This is discussed in the following steps
@@ -53,58 +70,153 @@ const AddSession = ( { id } : Props) => {
                     <Form.Control
                         type="number"
                         placeholder="The Sequence ID of the session (eg. 1, 2, 3...)"
-                        value={sequenceId}
-                        onChange={(event) => setSequenceId(event.target.value)}
+                        {...register('sequenceId', { required: true, pattern: /^\d+$/ })}
                     />
+                    {
+                        errors.sequenceId && (
+                            <div className="text-danger">
+                                {
+                                    errors.sequenceId?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.sequenceId?.type === 'pattern' && (
+                                        <div>Sequence ID must be a positive integer</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="name">
                     <Form.Label>Name</Form.Label>
                     <Form.Control
                         type="text"
                         placeholder="Name of the session, Eg. Introduction to Programming"
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}
+                        {...register('name', { required: true, pattern: /^[A-Za-z\d][A-Za-z\d .,'&_\/:+#@-]*$/ })}
                     />
+                    {
+                        errors.name && (
+                            <div className="text-danger">
+                                {
+                                    errors.name?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.name?.type === 'pattern' && (
+                                        <div>Name of the session has characters that are not allowed - Must begin with alphanumeric, and can have alphanumeric, spaces, and these characters only - .,'&_/:+#@-</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="speaker">
                     <Form.Label>Speaker</Form.Label>
                     <Form.Control
                         type="text"
                         placeholder="Name of the speaker(s). Eg. John Doe, Jane Doe"
-                        value={speaker}
-                        onChange={(event) => setSpeaker(event.target.value)}
+                        {...register('speaker', { required: true, pattern: /^[A-Za-z][A-Za-z ]*(,\s*[A-Za-z][A-Za-z ]*)*$/ })}
                     />
+                    {
+                        errors.speaker && (
+                            <div className="text-danger">
+                                {
+                                    errors.speaker?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.speaker?.type === 'pattern' && (
+                                        <div>Comma-separated name(s) of speaker(s)</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="duration">
                     <Form.Label>Duration</Form.Label>
                     <Form.Control
                         type="text"
                         placeholder="The duration of the session in hours (eg. 2.5)"
-                        value={duration}
-                        onChange={(event) => setDuration(event.target.value)}
+                        {...register('duration', { required: true, pattern: /^\d+(\.\d+)?$/ })}
                     />
+                    {
+                        errors.duration && (
+                            <div className="text-danger">
+                                {
+                                    errors.duration?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.duration?.type === 'pattern' && (
+                                        <div>Only number with optional decimal part allowed</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="level">
                     <Form.Label>Level</Form.Label>
                     <Form.Select
                         aria-label="Level"
-                        value={level}
-                        onChange={(event) => setLevel(event.target.value)}
+                        {...register('level', { required: true, validate: validateDurationAndLevel })}
                     >
                         <option disabled>-- Select the level --</option>
                         <option value="Basic">Basic</option>
                         <option value="Intermediate">Intermediate</option>
                         <option value="Advanced">Advanced</option>
                     </Form.Select>
+                    {
+                        errors.level && (
+                            <div className="text-danger">
+                                {
+                                    errors.level?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.level?.type === 'validate' && (
+                                        <div>The duration in insufficient for the selected level</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="abstract">
                     <Form.Label>Abstract</Form.Label>
                     <Form.Control
                         as="textarea"
                         rows={3}
-                        value={abstract}
-                        onChange={(event) => setAbstract(event.target.value)}
+                        {...register('abstract', { required: true, minLength: 20, maxLength: 1024 })}
                     />
+                    {
+                        errors.abstract && (
+                            <div className="text-danger">
+                                {
+                                    errors.abstract?.type === 'required' && (
+                                        <div>This field is required</div>
+                                    )
+                                }
+                                {
+                                    errors.abstract?.type === 'minLength' && (
+                                        <div>Minimum 20 characters needed</div>
+                                    )
+                                }
+                                {
+                                    errors.abstract?.type === 'maxLength' && (
+                                        <div>Maximum 1024 characters allowed</div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
                 </Form.Group>
 
                 <Button type="submit">Add a session</Button>
