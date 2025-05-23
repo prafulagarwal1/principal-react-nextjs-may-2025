4b22c5f91a8e24b8c83783e99946e7a997add31b
Prashanth Puranik <puranik@digdeeper.in>
2025-05-23 16:42:39 +0530

    Step 31: Create the form to add a session

--- 04-react/workshops-app/src/components/workshops/WorkshopDetails/AddSession/AddSession.tsx
+++ 04-react/workshops-app/src/components/workshops/WorkshopDetails/AddSession/AddSession.tsx
@@ -1,10 +1,72 @@
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 interface Props {
     id: number
 }
 
 const AddSession = ( { id } : Props) => {
     return (
-        <div>AddSession works!</div>
+        <div>
+            <h1 className="d-flex justify-content-between align-items-center">
+                Add a Session
+                <Link to=".." className="btn btn-primary">List of sessions</Link>
+            </h1>
+
+            <hr />
+
+            <Form>
+                <Form.Group className="mb-4" controlId="sequenceId">
+                    <Form.Label>Sequence ID</Form.Label>
+                    <Form.Control
+                        type="number"
+                        placeholder="The Sequence ID of the session (eg. 1, 2, 3...)"
+                        ref={sequenceIdRef}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Name of the session, Eg. Introduction to Programming"
+                    />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="speaker">
+                    <Form.Label>Speaker</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Name of the speaker(s). Eg. John Doe, Jane Doe"
+                    />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="duration">
+                    <Form.Label>Duration</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="The duration of the session in hours (eg. 2.5)"
+                    />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="level">
+                    <Form.Label>Level</Form.Label>
+                    <Form.Select
+                        aria-label="Level"
+                    >
+                        <option disabled>-- Select the level --</option>
+                        <option value="Basic">Basic</option>
+                        <option value="Intermediate">Intermediate</option>
+                        <option value="Advanced">Advanced</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="abstract">
+                    <Form.Label>Abstract</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                    />
+                </Form.Group>
+
+                <Button type="submit">Add a session</Button>
+            </Form>
+        </div>
     );
 };
 
