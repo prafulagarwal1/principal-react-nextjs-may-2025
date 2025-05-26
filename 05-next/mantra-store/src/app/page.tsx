7764d8efeb4d4f787e7df3fdbf181ba9a3c3ea17
Prashanth Puranik <puranik@digdeeper.in>
2025-05-26 15:40:58 +0530

    Step 6: Adding metadata

--- 05-next/mantra-store/src/app/page.tsx
+++ 05-next/mantra-store/src/app/page.tsx
@@ -1,5 +1,13 @@
 // @ -> src folder
 import Home from '@/components/home/home';
+import type { Metadata } from "next";
+
+// The metadata must be named as `metadata`
+export const metadata: Metadata = {
+    title: "Mantra Store Home",
+    description:
+        "Mantra Store Home - shop from our wide variety of products. Have them delivered within 2 hours at your doorstep.",
+};
 
 export default function HomePage() {
   return (
