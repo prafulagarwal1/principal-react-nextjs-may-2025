19b539a2ea4307cd7deb2d2a16a77d168ea7593e
Prashanth Puranik <puranik@digdeeper.in>
2025-05-26 18:04:41 +0530

    Step 10: A decent Home component (Optional step)

--- 05-next/mantra-store/src/app/page.tsx
+++ 05-next/mantra-store/src/app/page.tsx
@@ -10,10 +10,20 @@ export const metadata: Metadata = {
         "Mantra Store Home - shop from our wide variety of products. Have them delivered within 2 hours at your doorstep.",
 };
 
-export default function HomePage() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+// this is a server component, we can fetch data directly (DO NOT use useEffect)
+export default async function HomePage() {
+    // This fetch() is not the plain fetch() of Node JS - it is a wrapper around the plain fetch created by Next JS
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SITE_URL}/data/products-images.json`,
+        {
+            cache: "force-cache", // this is aNext JS - customized option
+        }
+    );
+    const images = await res.json();
+
+    return (
+      <>
+        <Home images={images} />
+      </>
+    );
 }
