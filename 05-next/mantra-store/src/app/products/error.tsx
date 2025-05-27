42497a4a4aa5d24d49b4fbc298bc1fea835dc8a5
Prashanth Puranik <puranik@digdeeper.in>
2025-05-27 16:25:27 +0530

    STEP 16: Showing an error page

---
+++ 05-next/mantra-store/src/app/products/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from 'next/link';
+
+// The error object passed by Next.js often includes a digest (a hashed error signature for debugging)
+interface Props {
+    error: Error & { digest?: string };
+    reset: () => void;
+}
+
+export default function Error({ error, reset }: Props) {
+    console.error("Page error:", error);
+
+    return (
+        <div>
+            <h2>Something went wrong.</h2>
+            <p>{error.message}</p>
+            <button onClick={reset}>Try again</button>
+            <Link href="/">Go home</Link>
+        </div>
+    );
+}
--- 05-next/mantra-store/src/app/products/page.tsx
+++ 05-next/mantra-store/src/app/products/page.tsx
@@ -16,15 +16,35 @@ type GetProductsResponse = {
 // By default a page is rendered using SSG (we see later how to switch to SSR when using DB calls in place of external API calls using fetch())
 export default async function ProductsPage() {
     try {
+        // const {
+        //     count,
+        //     page,
+        //     products
+        // } : GetProductsResponse = await getProducts();
+
+        const response = await fetch( `${process.env.NEXT_STORE_API_URL}/api/product`,
+            /*
+            {
+                cache: 'force-cache'
+            }
+            */
+        );
+
         const {
             count,
             page,
             products
-        } : GetProductsResponse = await getProducts();
+        } : GetProductsResponse = await response.json();
+
+        if ( !response.ok ) {
+            throw new Error( 'Unable o fetch products' );
+        }
 
         return <ProductsList count={count} page={page} products={products} />;
     } catch( error ) {
         console.log( (error as Error).message );
-        return <div>Failed to load products. Please try again later.</div>;
+        // return <div>Failed to load products. Please try again later.</div>;
+
+        throw new Error("Failed to load products. Please try again later.");
     }
 }
