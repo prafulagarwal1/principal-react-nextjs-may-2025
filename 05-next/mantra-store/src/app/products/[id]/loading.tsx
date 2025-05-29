bec233b8b9dbd126be115637ca9853668c44c921
Prashanth Puranik <puranik@digdeeper.in>
2025-05-29 14:43:06 +0530

    STEP 19: Showing loading components when page loads

---
+++ 05-next/mantra-store/src/app/products/[id]/loading.tsx
@@ -0,0 +1,6 @@
+// backend will stream this loading page, while the product detail page is being constructed
+// The Page componant is wrapped in like this (where ProductDetailPage is lazily loaded) - <Suspense fallback={<Loading />}><ProductDetailPage /><Suspense>
+// It uses this Loading component as a
+export default function Loading() {
+    return <p>Loading product...</p>;
+}
